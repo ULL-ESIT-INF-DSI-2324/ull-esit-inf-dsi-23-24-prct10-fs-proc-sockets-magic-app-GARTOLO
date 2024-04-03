407dbb03316f0ff5a4a4e867e4fd247bda430a76
Guillermo Gonzalez <alu0101466941@ull.edu.es>
2024-04-03 10:21:06 +0000

    feat: modification

---
+++ src/mod/client.ts
@@ -0,0 +1,18 @@
+import net from 'net';
+
+if (process.argv.length !== 3) console.log('Please, provide a filename.');
+else {
+  const client = net.connect({port: 8000});
+  const filename = process.argv[2];
+  console.log('Filename:', filename)
+  
+  // wc -l lines, wc -m char, wc -w words
+
+  const jsonData = JSON.stringify({ filename: filename});
+
+  client.write(jsonData);
+
+  client.on('data', (data) => {
+    console.log(data.toString())
+  })
+}
